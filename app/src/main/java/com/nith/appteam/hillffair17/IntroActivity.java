03fc0fc5d65c8ccf4ebcc4058b7be5dba60fe3d6
octacode <allblue.shasha@gmail.com>
2017-08-25 22:11:08 +0530

    Adds intro activity

---
+++ app/src/main/java/com/nith/appteam/hillffair17/IntroActivity.java
@@ -0,0 +1,40 @@
+package com.nith.appteam.hillffair17;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntro2Fragment;
+
+/**
+ * Created by shasha on 25/8/17.
+ */
+
+public class IntroActivity extends AppIntro2 {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*Note-@octacode: Adds the App intro fragments*/
+        addSlide(AppIntro2Fragment.newInstance("Hillfair", getString(R.string.dummy_desc), R.mipmap.ic_launcher, Color.DKGRAY));
+        addSlide(AppIntro2Fragment.newInstance("Hillfair", getString(R.string.dummy_desc), R.mipmap.ic_launcher, Color.YELLOW));
+        addSlide(AppIntro2Fragment.newInstance("Hillfair", getString(R.string.dummy_desc), R.mipmap.ic_launcher, Color.MAGENTA));
+        addSlide(AppIntro2Fragment.newInstance("Hillfair", getString(R.string.dummy_desc), R.mipmap.ic_launcher, Color.RED));
+        addSlide(AppIntro2Fragment.newInstance("Hillfair", getString(R.string.dummy_desc), R.mipmap.ic_launcher, Color.BLUE));
+        addSlide(AppIntro2Fragment.newInstance("Hillfair", getString(R.string.dummy_desc), R.mipmap.ic_launcher, Color.CYAN));
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
--- app/src/main/java/com/nith/appteam/hillffair17/MainActivity.java
+++ app/src/main/java/com/nith/appteam/hillffair17/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nith.appteam.hillffair17;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, IntroActivity.class));
     }
 }
