bc0f1305923dfe6d723b38f0cd90e5977e6601d3
kartik <kartiksaxena500@gmail.com>
2017-10-13 20:22:13 +0530

    Added AppIntro

--- app/src/main/java/com/nith/appteam/hillffair17/Utils/SharedPref.java
+++ app/src/main/java/com/nith/appteam/hillffair17/Utils/SharedPref.java
@@ -1,6 +1,7 @@
 package com.nith.appteam.hillffair17.Utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.nith.appteam.hillffair17.Activities.BattleDayActivity;
 import com.nith.appteam.hillffair17.Activities.ClubActivity;
@@ -14,9 +15,17 @@ import static android.content.Context.MODE_PRIVATE;
  */
 
 public class SharedPref {
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private static final String PREF_NAME="UserInfo";
+    private static final String IS_FIRST_TIME="isfirstTime";
+
     public SharedPref(EventActivity eventActivity) {
     }
-
+    public SharedPref(Context context){
+        sharedPreferences = context.getSharedPreferences(PREF_NAME,Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
     public SharedPref(BattleDayActivity battleDayActivity) {
     }
 
@@ -24,10 +33,18 @@ public class SharedPref {
     }
 
     public static boolean getFirstRun(Context context) {
-        return false;
+        return context.getSharedPreferences(context.getString(R.string.preference), MODE_PRIVATE).getBoolean(context.getString(R.string.is_first_run), true);
     }
 
     public static void setFirstRun(Context context) {
         context.getSharedPreferences(context.getString(R.string.preference), MODE_PRIVATE).edit().putBoolean(context.getString(R.string.is_first_run), false).apply();
+    }
+
+    public boolean showIsFirstTime(){
+        return sharedPreferences.getBoolean(IS_FIRST_TIME,false);
+    }
+    public void setIsFirstTime(){
+        editor.putBoolean(IS_FIRST_TIME,true);
+        editor.commit();
     }
 }
