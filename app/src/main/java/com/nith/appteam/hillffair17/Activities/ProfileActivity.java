033121012b214311e4eae803df6c40a8a14017c9
Aditya Arora <aadityaaaaroraa@gmail.com>
2017-10-17 22:50:48 +0530

    Profile Activity added

---
+++ app/src/main/java/com/nith/appteam/hillffair17/Activities/ProfileActivity.java
@@ -0,0 +1,101 @@
+package com.nith.appteam.hillffair17.Activities;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.ImageViewTarget;
+import com.nith.appteam.hillffair17.Adapters.ProfilePagerAdapter;
+import com.nith.appteam.hillffair17.Fragments.ProfileTab1;
+import com.nith.appteam.hillffair17.Fragments.ProfileTab2;
+import com.nith.appteam.hillffair17.Fragments.ProfileTab3;
+import com.nith.appteam.hillffair17.R;
+import com.nith.appteam.hillffair17.Utils.SharedPref;
+
+import java.util.ArrayList;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    //For testing.
+    private static final String IMG_URL = "";
+
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private ImageView coverImage;
+    private ImageView profilePic;
+    private SharedPref sharedPref;
+
+
+    private void findViews(){
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        coverImage = (ImageView) findViewById(R.id.cover);
+        profilePic = (ImageView) findViewById(R.id.profilePic);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        findViews();
+
+        sharedPref = new SharedPref(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        collapsingToolbarLayout.setTitle(sharedPref.getUserName());
+        toolbar.setTitle(sharedPref.getUserName());
+
+            Glide.with(this).load(sharedPref.getUserPicUrl()).asBitmap().diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.person_icon).error(R.drawable.person_icon).into(new ImageViewTarget<Bitmap>(profilePic) {
+                @Override
+                protected void setResource(Bitmap resource) {
+                    RoundedBitmapDrawable drawable= RoundedBitmapDrawableFactory.create(getResources(),resource);
+                    drawable.setCircular(true);
+                    profilePic.setImageDrawable(drawable);
+                }
+            });
+
+
+
+
+        ArrayList<Fragment> fragmentArrayList=new ArrayList<>();
+        fragmentArrayList.add(new ProfileTab1());
+        fragmentArrayList.add(new ProfileTab2());
+        fragmentArrayList.add(new ProfileTab3());
+
+
+        ArrayList<String> titleArrayList=new ArrayList<>();
+        titleArrayList.add("Events");
+        titleArrayList.add("Basic info");
+        titleArrayList.add("News Feed");
+        
+
+        ProfilePagerAdapter adapter = new ProfilePagerAdapter(getSupportFragmentManager(),fragmentArrayList,titleArrayList);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(1).select();
+        viewPager.setOffscreenPageLimit(3);
+
+
+    }
+}
--- app/src/main/java/com/nith/appteam/hillffair17/Utils/SharedPref.java
+++ app/src/main/java/com/nith/appteam/hillffair17/Utils/SharedPref.java
@@ -17,8 +17,17 @@ import static android.content.Context.MODE_PRIVATE;
 public class SharedPref {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+
     private static final String PREF_NAME="UserInfo";
+    private static final String LOGIN_STATUS="loginstatus";
     private static final String USER_ID="apikey";
+    private static final String IS_FIRST_TIME="isfirstTime";
+    private static final  String USER_NAME="name";
+    private static  final String USER_EMAIL="email";
+    private static  final String USER_ROLLNO="rollno";
+    private static  final String USER_PIC_URL="picUrl";
+    private static final String NITIAN_STATUS="nitian";
+    private static  final String FIRST_ROLL_REGISTER="rollRegister";
 
     public SharedPref(Context context){
         sharedPreferences = context.getSharedPreferences(PREF_NAME,Context.MODE_PRIVATE);
@@ -34,6 +43,20 @@ public class SharedPref {
     public SharedPref(ClubActivity clubActivity) {
     }
 
+    public void setLoginStatus(boolean isLogIn){
+        editor.putBoolean(LOGIN_STATUS,isLogIn);
+        editor.commit();
+    }
+
+    public boolean getInstructionsReadStatus(){
+        return sharedPreferences.getBoolean("quizinstruct",false);
+    }
+
+    public boolean getLoginStatus(){
+        return sharedPreferences.getBoolean(LOGIN_STATUS,false);
+    }
+
+
     public static boolean getFirstRun(Context context) {
         return false;
     }
@@ -48,5 +71,65 @@ public class SharedPref {
     }
     public String getUserId(){
         return sharedPreferences.getString(USER_ID,"");
+    }
+
+    public void setIsFirstTime(){
+        editor.putBoolean(IS_FIRST_TIME,true);
+        editor.commit();
+    }
+
+    public boolean showIsFirstTime(){
+        return sharedPreferences.getBoolean(IS_FIRST_TIME,false);
+    }
+
+    public void setUserName(String name){
+        editor.putString(USER_NAME,name);
+        editor.commit();
+    }
+
+    public String getUserName(){
+        return sharedPreferences.getString(USER_NAME,"");
+    }
+
+    public void setUserRollno(String rollno){
+        editor.putString(USER_ROLLNO,rollno);
+        editor.commit();
+    }
+    public String getUserRollno(){
+        return sharedPreferences.getString(USER_ROLLNO,"");
+    }
+
+
+    public void setUserEmail(String email){
+        editor.putString(USER_EMAIL,email);
+        editor.commit();
+    }
+    public String getUserEmail(){
+        return sharedPreferences.getString(USER_EMAIL,"");
+    }
+
+    public void setUserPicUrl(String picUrl){
+        editor.putString(USER_PIC_URL,picUrl);
+        editor.commit();
+    }
+    public String getUserPicUrl(){
+        return sharedPreferences.getString(USER_PIC_URL,"");
+    }
+
+    public void  setNitianStatus(boolean status){
+        editor.putBoolean(NITIAN_STATUS,status);
+        editor.commit();
+    }
+
+    public boolean getNitianStatus(){
+        return sharedPreferences.getBoolean(NITIAN_STATUS,false);
+    }
+
+    public void  setFirstRollRegister(boolean status){
+        editor.putBoolean(FIRST_ROLL_REGISTER,status);
+        editor.commit();
+    }
+    public boolean getFirstTimeRollregister(){
+        return sharedPreferences.getBoolean(FIRST_ROLL_REGISTER,true);
     }
 }
